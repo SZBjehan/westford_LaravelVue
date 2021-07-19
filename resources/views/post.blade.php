16f6bcb3c4b52cdc48cee80d79661038e34c76ca
syed ziaul bin bashar <syed.ziaul.bin.bashar@g.bracu.ac.bd>
2021-07-19 07:14:18 +0600

    first

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Westford</title>
         <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
         <meta name="csrf-token" value="{{ csrf_token() }}" />
     </head>
